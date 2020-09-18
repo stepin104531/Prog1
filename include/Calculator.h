60427564010327a062fd76aa68a7670dc176cc2f
stepin104531 <39698285+stepin104531@users.noreply.github.com>
2020-09-18 16:38:48 +0530

    Create Calculator.h

---
+++ include/Calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H_INCLUDED
+#define CALCULATOR_H_INCLUDED
+
+int sum(int,int);
+int sub(int,int);
+int mul(int,int);
+int div(int,int);
+int mod(int,int);
+
+#endif // CALCULATOR_H_INCLUDED
